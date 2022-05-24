33abf0af6c5f73c5e4a6936ca5756828291df1be
Veronika-Ryabchun <nikakyiv696@gmail.com>
2022-05-24 21:29:38 +0300

    Додала 3 роботу

---
+++ Лабораторна 13.1/Лабораторна 13.2/Лабораторна 13.2/Avtobusi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_13._2
+{
+    class Malenkiy : Avtobus
+    {
+        private int sidyachee;
+        public Malenkiy(int sidyachee, string nomer, int spojivannya) : base(nomer, spojivannya)
+        {
+            this.sidyachee = sidyachee;
+        }
+        public override int miscya
+        {
+            get
+            {
+                return sidyachee;
+            }
+        }
+    }
+    class Seredniy : Avtobus
+    {
+        private int sidyachee;
+        private int stoyachee;
+        public Seredniy(int sidyachee, int stoyachee, string nomer, int spojivannya) : base(nomer, spojivannya)
+        {
+            this.sidyachee = sidyachee;
+            this.stoyachee = stoyachee;
+        }
+        public override int miscya
+        {
+            get
+            {
+                return sidyachee + stoyachee;
+            }
+        }
+    }
+    class Velykiy : Avtobus
+    {
+        private int sidyachee;
+        private int stoyachee;
+        private int invalidnoe;
+        public Velykiy(int sidyachee, int stoyachee, int invalidnoe, string nomer, int spojivannya) : base(nomer, spojivannya)
+        {
+            this.sidyachee = sidyachee;
+            this.stoyachee = stoyachee;
+            this.invalidnoe = invalidnoe;
+        }
+        public override int miscya
+        {
+            get
+            {
+                return sidyachee + stoyachee + invalidnoe;
+            }
+        }
+    }
+}
---
+++ Лабораторна 13.1/Лабораторна 13.2/Лабораторна 13.2/Avtostancia.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_13._2
+{
+    class Avtostancia
+    {
+        private Avtobus[] busiki;
+        public Avtostancia(Avtobus[] busiki)
+        {
+            this.busiki = busiki;
+
+        }
+        public int Kilkict()
+        {
+            return busiki.Length;
+        }
+        public Avtobus find(string nomer)
+        {
+            for (int i = 0; i < busiki.Length; i++)
+            {
+                if (nomer == busiki[i].nomer)
+                    return busiki[i];
+            }
+            return null;
+        }
+        public Avtobus[] find(int ot, int po)
+        {
+            int a = 1;
+            Avtobus[] vybir = new Avtobus[a];
+            for (int i = 0; i < busiki.Length; i++)
+            {
+                if ((ot <= busiki[i].spojivannya) && (busiki[i].spojivannya <= po))
+                {
+                    vybir[a - 1] = busiki[i];
+                    a++;
+                    Array.Resize(ref vybir, a);
+                }
+            }
+            return vybir;
+        }
+        public void Print()
+        {
+            foreach (Avtobus a in busiki)
+            {
+                Console.WriteLine(a.ToString());
+            }
+        }
+        public void Sort()
+        {
+            Array.Sort(busiki);
+        }
+    }
+}
+
